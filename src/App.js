fa5ac73e31ec6b0c480b959362e9eeda348faad2
Gilang Al Khansa <gilang.alkhansa@iproperty.com>
2019-06-29 09:07:40 +0700

    Set up the container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from 'antd';
+import { Button } from 'antd';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 425px;
+  display: block;
+  position: relative;
+  margin: 0 auto;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Container>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -14,7 +23,7 @@ class App extends Component {
         <p className="App-intro">
         <Button type="primary">Button</Button> Ant Design Button with primary color defined in ant-theme-vars.less 
         </p>
-      </div>
+      </Container>
     );
   }
 }
